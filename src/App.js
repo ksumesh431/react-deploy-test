746bb7570ad71d4a14fd17f0f3b2eafe0b62bb4d
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-02-25 20:09:55 +0530

    s3 artifacts test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New changes to app js to test build
+          artifacts etst
         </p>
         <a
           className="App-link"
