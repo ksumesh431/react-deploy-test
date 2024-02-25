3ad46c45c4fffa2a05fe1738793ec2ce8647fd1f
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-02-25 21:16:47 +0530

    cdk pipeline deploy test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          artifacts etst
+          release test through cdk pipeline
         </p>
         <a
           className="App-link"
